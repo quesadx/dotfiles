22ecd63bf897837cdb9d44699feeb89d06620a84
Matteo Quesada <matteo.vargas.quesada@est.una.ac.cr>
2026-01-31 02:55:31 -0600

    Pre-overview checkpoint

--- nixos-dotfiles/home.nix
+++ nixos-dotfiles/home.nix
@@ -91,6 +91,13 @@
 
   programs.home-manager.enable = true;
 
+  wayland.windowManager.hyprland = {
+    enable = true;
+    plugins = [
+      pkgs.hyprlandPlugins.hyprexpo
+    ];
+  };
+
 ##################################
 ### HOME MANAGER STATE VERSION ###
 ##################################
