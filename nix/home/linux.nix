6b356c69e37801f83237bcbe1a2d6e5ad70ab8ee
Matteo Quesada <matteo.vargas.quesada@est.una.ac.cr>
2026-08-16 21:06:32 -0600

    feat: firefox-devedition

--- nix/home/linux.nix
+++ nix/home/linux.nix
@@ -29,6 +29,23 @@ in
     ];
   };
 
+  programs.firefox = {
+    enable = true;
+    package = pkgs.firefox-devedition;
+    policies = {
+      ExtensionSettings = {
+        "uBlock0@raymondhill.net" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        };
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+        };
+      };
+    };
+  };
+
   programs.chromium = {
     enable = true;
     extensions = [
